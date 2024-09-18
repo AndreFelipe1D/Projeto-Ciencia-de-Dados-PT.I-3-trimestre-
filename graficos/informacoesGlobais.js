ce06fe3f81e4b770d5eba31537417894b9ea3ca5
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-09-18 02:13:22 +0000

    alteracoes

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,6 +5,9 @@ console.log(url);
 async function visualizarinformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json()
-    console.log(dados.tempo_medio);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conetadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+    console.log(paragrafo)
 }
 visualizarinformacoesGlobais()
