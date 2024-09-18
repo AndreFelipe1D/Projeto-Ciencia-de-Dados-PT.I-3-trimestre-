f46a380b72408e0941a0344fea7ab0a65c579ada
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-09-18 02:20:45 +0000

    alteracoes

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,7 +7,7 @@ async function visualizarinformacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conetadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conetadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
