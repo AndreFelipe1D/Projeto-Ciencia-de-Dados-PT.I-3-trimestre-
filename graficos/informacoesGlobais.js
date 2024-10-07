80489f19955ae5efddf6a7bb0b7ef1dfc0695ec9
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-10-07 14:26:35 +0000

    alteracoes

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,6 +8,7 @@ async function visualizarinformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_conectadas)/ 1e9
     const pessoasNoMundo = (dados.total_pessoas_mundo)/ 1e9
     const horas = parseInt (dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio-horas)* 100)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> estão conetadas em alguma rede social e passam em média <span>${horas}</span> horas conectadas.`
