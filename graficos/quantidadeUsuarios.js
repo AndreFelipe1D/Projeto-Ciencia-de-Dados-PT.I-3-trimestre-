126971fec9bc572fe08ee342b5dfac8d9cb33ccc
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-10-12 23:44:37 +0000

    alteracoes

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -19,7 +19,8 @@ async function quantidadeUsuarios() {
     ]
 
     const layout = {
-        plot_bgcolor: getCSS('--bg-color')
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
     }
 
     const grafico = document.createElement('div')
