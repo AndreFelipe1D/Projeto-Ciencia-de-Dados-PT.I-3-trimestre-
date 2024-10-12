818c58e87b0171a7e4d4843efa41dc1db8f65800
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-10-12 03:23:47 +0000

    alteracoes

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,7 +1,5 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
-
 async function visualizarinformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json()
