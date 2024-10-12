b14e31024dd427f1494374a2e50d8522e0e422bd
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-10-12 03:14:03 +0000

    alteracoes

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
 
 console.log(url);
 
